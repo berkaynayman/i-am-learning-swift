ffd791fbe0be7d7deee64bc714553be8c1f87008
Berkay Nayman <berkaynayman4@gmail.com>
2024-12-06 09:33:27 +0100

    Hello World
    I am just started the use xcode. I was create a Text and added padding, background.

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Berkay Nayman on 6.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
